466e86d5d834df44c4588fe41c03871025abe818
dominikjagoda <dominik.jagoda881@gmail.com>
2022-10-23 17:51:23 +0200

    Feature 1

--- hellowrld.py
+++ hellowrld.py
@@ -1,13 +1,15 @@
 name = "Hello world zmiana by Dominik"
 print(name)
 
+def div(a, b):
+    return a / b
 
-def add()
-	z = 2+2
-	return z
+def add(a, b)
+	return a + b
+	
 
 def print_name(name):
-  print(name)
+    print(name)
   
 print_name()
 
